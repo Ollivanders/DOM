f62b1d0a4604e111d4602ea2db06842d483cf0b7
Ollivanders <ollivander64@gmail.com>
2023-06-02 21:01:03 +0100

    ➕ ADD: zsh config

--- zsh/custom/config.zsh
+++ zsh/custom/config.zsh
@@ -37,3 +37,27 @@ bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+
+################################################################################
+# autocomplete
+LINE_LENGTH=32
+
+zstyle ':autocomplete:*' list-lines $LINE_LENGTH  # int
+# If there are fewer than this many lines below the prompt, move the prompt up
+# to make room for showing this many lines of completions (approximately).
+
+zstyle ':autocomplete:history-search:*' list-lines $LINE_LENGTH  # int
+# Show this many history lines when pressing ↑.
+
+zstyle ':autocomplete:history-incremental-search-*:*' list-lines $LINE_LENGTH  # int
+# Show this many history lines when pressing ⌃R or ⌃S.
+
+zstyle ':autocomplete:*' insert-unambiguous yes
+# no:  Tab inserts the top completion.
+# yes: Tab first inserts a substring common to all listed completions, if any.
+
+zstyle ':autocomplete:*' widget-style menu-select
+# complete-word: (Shift-)Tab inserts the top (bottom) completion.
+# menu-complete: Press again to cycle to next (previous) completion.
+# menu-select:   Same as `menu-complete`, but updates selection in menu.
+# ⚠️ NOTE: This setting can NOT be changed at runtime.
